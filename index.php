24be8b5bdc10ff4d588abf65af6f9d7482396414
Seong-Jun Yea <44723150+kmlagalbi@users.noreply.github.com>
2021-08-02 02:29:12 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+  //http://localhost:8080/
+  //echo "hello"
+?>
+<!DOCTYPE html>
+<html>
+  <body>
+  
+    How to use Crawler<br><br>
+    1. load roadAddress dataset by load.php<br>
+    2. find the roadAddress index number range of town you will search<br>
+    3. change navdb settings and execute navdb.php<br>
+    4. check manually by navc.php
+
+    <!--
+    <a href="insert.php">insert</a>
+    <br>
+    <a href="jsonread.php">jsonread</a>
+    -->
+  </body>
+</html>
